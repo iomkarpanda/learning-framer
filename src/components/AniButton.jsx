2eed759a22963209a42dd23742ace388173d403a
Omkar Panda <pandaomkar26@gmail.com>
2025-12-27 15:04:47 +0530

    worked on inital animate

--- src/components/AniButton.jsx
+++ src/components/AniButton.jsx
@@ -1,14 +1,17 @@
 import {motion} from "motion/react";
-
+import '../index.css';
 function AniButton(){
     return(
-        <motion.div className="w-24 h-10 bg-slate-300 flex justify-center items-center rounded"
-                    initial={{opacity:1}}
-                    animate = {{opacity:0.2}}
-                    transition={{duration:1,ease:'easeIn'}}
+        <motion.button className="w-24 h-10 flex justify-center items-center rounded cursor-pointer border border-gray-400 shadow-md transition-colors"
+                    // initial={{backgroundColor:"#808080"}}
+                    // animate={{backgroundColor: ["#808080", "#ff0000", "#0000ff"]}}
+                    initial={{scale:1,x:0,y:0,borderWidth:"4"}}
+                    whileHover={{scale:1.05,}}
+                    transition={{duration:0.3, ease:"linear",}}
+                    style={{borderStyle:'solid'}}
         >
             Hello World
-        </motion.div>
+        </motion.button>
     )
 }
 
