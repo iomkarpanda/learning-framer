e7f02c77f244ea1cbd478d10140a45d8bfdd9f29
Omkar Panda <pandaomkar26@gmail.com>
2025-12-29 21:57:58 +0530

    implemented stagger animation

--- src/components/VariantAni.jsx
+++ src/components/VariantAni.jsx
@@ -2,22 +2,59 @@ import {motion} from "motion/react"
 import "../App.css"
 function VariantAni(){
 
-    const ChildVariant = {
-        start:{opacity:1},
-        end: {opacity:0.3},
+const ParentVariant = {
+  start: {},
+  end: {
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.3
     }
+  }
+};
 
+const ChildVariant = {
+  start: {
+    opacity: 0,
+    y: 20
+  },
+  end: {
+    opacity: 1,
+    y: 0
+  }
+};
 
-    return(
-        <motion.div className="parent w-full h-175 flex flex-col justify-center items-center gap-2 bg-sky-400">
-            <motion.div className="box" variants={ChildVariant}> div1 </motion.div>
-            <motion.div className="box" variants={ChildVariant}> div2 </motion.div>
-            <motion.div className="box" variants={ChildVariant}> div3 </motion.div>
-            <motion.div className="box" variants={ChildVariant}> div4 </motion.div>
-            <motion.div className="box" variants={ChildVariant}> div5 </motion.div>
-            <motion.div className="box" variants={ChildVariant}> div6 </motion.div>
-        </motion.div>
-    )
+return (
+<motion.div
+  className="parent w-full h-175 flex flex-col justify-center items-center gap-2 bg-sky-400"
+  variants={ParentVariant}
+  initial="start"
+  animate="end"
+>
+  <motion.div className="box" variants={ChildVariant}>div1</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div2</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div3</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+  <motion.div className="box" variants={ChildVariant}>div4</motion.div>
+</motion.div>
+);
+
 }
 
 export default VariantAni;
